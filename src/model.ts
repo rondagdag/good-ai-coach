0bce135d4ca4a4407a3dc512758a895949a204ed
rondagdag <rlyle78@gmail.com>
2024-11-24 19:09:46 +0800

    add github models

--- src/model.ts
+++ src/model.ts
@@ -1,13 +1,23 @@
 import { initChatModel } from "langchain/chat_models/universal";
 
+//github
+export const model = await initChatModel("gpt-4", {
+  modelProvider: "openai",
+  apiKey: "GITHUB_OPENAI_API_KEY",
+  temperature: 0,
+  configuration: {
+    baseURL: 'https://models.inference.ai.azure.com'
+  }
+});
+
 // export const model = await initChatModel("llama3.2", {
 //   modelProvider: "ollama",
 //   temperature: 0,
 // });
-export const model = await initChatModel("gpt-4", {
-  modelProvider: "azure_openai",
-  temperature: 0,
-});
+// export const model = await initChatModel("gpt-4", {
+//   modelProvider: "azure_openai",
+//   temperature: 0,
+// });
 // export const model = await initChatModel("gpt-4", {
 //   modelProvider: "openai",
 //   temperature: 0,
